7a65ad5c09d52253e2c77b318878f98d0ef15a76
Risabh Rai <rishabhrai_20150@aitpune.edu.in>
2022-01-23 13:23:36 +0530

    adding router for navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import Home from "../src/components/HomePage/index";
 import Schedule from "../src/components/Schedule/index";
@@ -8,140 +8,50 @@ import ContactUs from "../src/components/ContactUs/index";
 import TeamMembers from "./components/Team";
 
 import "./App.scss"
+import ScrollToTop from "./components/ScrollToTop";
 
-import { motion } from "framer-motion";
-
-const choiceClickHandler = (setChoice, option) => {
-	setChoice(option);
-}
-
-const renderChoice = (choice) => {
-	switch (choice) {
-		case "home":
-			return (
-				<>
-					<motion.div
-						key={choice}
-						initial={{ opacity: 0.01 }}
-						animate={{ opacity: 1 }}
-						transition={{
-							delay: 0.1,
-							duration: 0.8
-						}}
-					>
-						<Home />
-					</motion.div>
-				</>
-			);
-		case "schedule":
-			return (
-				<>
-					<motion.div
-						key={choice}
-						initial={{ opacity: 0.01 }}
-						animate={{ opacity: 1 }}
-						transition={{
-							delay: 0.1,
-							duration: 0.8
-						}}
-					>
-						<Schedule />
-					</motion.div>
-				</>
-			);
-		case "posts":
-			return (
-				<>
-					<motion.div
-						key={choice}
-						initial={{ opacity: 0.01 }}
-						animate={{ opacity: 1 }}
-						transition={{
-							delay: 0.1,
-							duration: 0.8
-						}}
-					>
-						<PostSection />
-					</motion.div>
-				</>
-			);
-		case "contactUs":
-			return (
-				<>
-					<motion.div
-						key={choice}
-						initial={{ opacity: 0.01 }}
-						animate={{ opacity: 1 }}
-						transition={{
-							delay: 0.1,
-							duration: 0.8
-						}}
-					>
-						<ContactUs />
-					</motion.div>
-				</>
-			);
-		case "team":
-			return (
-				<>
-					<motion.div
-						key={choice}
-						initial={{ opacity: 0.01 }}
-						animate={{ opacity: 1 }}
-						transition={{
-							delay: 0.1,
-							duration: 0.8
-						}}
-					>
-						<TeamMembers />
-					</motion.div>
-				</>
-			);
-		default:
-			break;
-	}
-}
 
 function App() {
-	const [choice, setChoice] = useState("home");
-
 	return (
-		<div className="App">
-			<section className="choice-navbar">
-				<div className="button-list">
-					<button className="home nav-btn" onClick={() => choiceClickHandler(setChoice, "home")}>
-						<i className="icon fa fa-home" id="uno"></i>
-					</button>
-					<button className="schedule nav-btn" onClick={() => choiceClickHandler(setChoice, "schedule")}>
-						<i
-							className="icon fa fa-calendar"
-							id="dos"
-						></i>
-					</button>
-					<button className="posts nav-btn" onClick={() => choiceClickHandler(setChoice, "posts")}>
-						<i
-							className="icon fa fa-user-friends"
-							id="cuatro"
-						></i>
-					</button>
-					<button className="contactUs nav-btn" onClick={() => choiceClickHandler(setChoice, "contactUs")}>
-						<i
-							className="icon fa fa-comments"
-							id="pent"
-						></i>
-					</button>
-					<button className="team nav-btn" onClick={() => choiceClickHandler(setChoice, "team")}>
-						<i
-							className="icon fa fa-comments"
-							id="pent"
-						></i>
-					</button>
+		<Router>
+			<ScrollToTop>
+				<div className="App">
+					<section className="choice-navbar">
+						<div className="button-list">
+							<button className="home nav-btn" > <Link to="/" >
+								<i className="icon fa fa-home" id="uno"></i>
+							</Link>
+							</button>
+							<button className="schedule nav-btn" > <Link to="/schedule" >
+								<i className="icon fa fa-calendar" id="dos"></i>
+							</Link>
+							</button>
+							<button className="posts nav-btn" > <Link to="posts" >
+								<i className="fas fa-clone"></i>
+							</Link>
+							</button>
+							<button className="contactUs nav-btn" > <Link to="contactus" >
+								<i className="fas fa-address-book"></i>
+							</Link>
+							</button>
+							<button className="team nav-btn" > <Link to="ourteam" >
+								<i className="fas fa-users"></i>
+							</Link>
+							</button>
+						</div>
+					</section>
+					<section className="content">
+						<Routes>
+							<Route exact path='/' element={<Home />}></Route>
+							<Route exact path='/schedule' element={<Schedule />}></Route>
+							<Route exact path='/posts' element={<PostSection />}></Route>
+							<Route exact path='/contactus' element={<ContactUs />}></Route>
+							<Route exact path='/ourteam' element={<TeamMembers />}></Route>
+						</Routes>
+					</section>
 				</div>
-			</section>
-			<section className="content">
-				{renderChoice(choice)}
-			</section>
-		</div>
+			</ScrollToTop>
+		</Router >
 	);
 }
 
